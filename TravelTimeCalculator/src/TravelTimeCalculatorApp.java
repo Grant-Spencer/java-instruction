777a84c6a720bb437e2afc1d28bb38e567a259ef
Grant Spencer <gwspence22@gmail.com>
2020-09-09 21:35:36 -0400

    additional examples

---
+++ TravelTimeCalculator/src/TravelTimeCalculatorApp.java
@@ -0,0 +1,9 @@
+
+public class TravelTimeCalculatorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ch03_instruction/src/Instruction_App.java
+++ ch03_instruction/src/Instruction_App.java
@@ -1,18 +1,41 @@
-
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 public class Instruction_App {
 
-	public static void main(String[] args) {
-		double curVal = 5000;
-		double monthInv = 100;
-		double yearlyIntRate = 0.12;
-	
-	curVal = (curVal + monthInv) * (1 + yearlyIntRate / 12);
-	
-	System.out.println(curVal);
+	public static void main(String[] args {
+		BigDecimal myBD = new BigDecimal(3.1459);
+		BigDecimal pi = new BigDecimal(56.0);
+		
+		myBD.add(pi);
+		
+		System.out.println(myBD);
+		
 	}
 	
 	
+//	public static void main(String[] args) {
+//		double percent = 0.505;
+//		NumberFormat perFormatter = NumberFormat.getPercentInstance();
+//		String formattedPercent = perFormatter.format(percent);
+//		System.out.println(formattedPercent);
+//	}
+//
+//
+//	}
+//	
+//	
+//	public static void main(String[] args) {
+//		double curVal = 5000;
+//		double monthInv = 100;
+//		double yearlyIntRate = 0.12;
+//	
+//	curVal = (curVal + monthInv) * (1 + (yearlyIntRate / 12));
+//	
+//	System.out.println(curVal);
+//	}
+//	
+//	
 //public static void main(String[] args) {
 //		char letter1 = 'C';
 //		
