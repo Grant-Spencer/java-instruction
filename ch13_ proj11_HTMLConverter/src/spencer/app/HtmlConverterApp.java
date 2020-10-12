3c9215fe03f3488005bded41a65050bce970219c
Grant Spencer <gwspence22@gmail.com>
2020-10-12 17:58:40 -0400

    October instruction

--- ch13_ proj11_HTMLConverter/src/spencer/app/HtmlConverterApp.java
+++ ch13_ proj11_HTMLConverter/src/spencer/app/HtmlConverterApp.java
@@ -3,26 +3,30 @@ package spencer.app;
 public class HtmlConverterApp {
 
 	public static void main(String[] args) {
-		String html = "<h1>Grocery List</h1>\n"
-
-				+ "<ul>\n" + "<li>Eggs</li>\n" + "<li>Milk</li>\n" + "<li>Butter</li>\n" + "</ul>";
+		String html = "<h1>Grocery List</h1>\n" + "<ul>\n" + "<li>Eggs</li>\n" + "<li>Milk</li>\n" + "<li>Butter</li>\n"
+				+ "</ul>";
 
 		System.out.println("HTML Input:\n" + html);
 
+		// remove spaces before tags
 		while (html.indexOf(" <") != -1) {
 			html = html.replace(" < ", "<");
 		}
+		
+		// replace list items with asterisks
 		html = html.replace("<li>", "* ");
 
+		StringBuilder htmlBldr = new StringBuilder(html);
+		
 		int beginTagIndex = 0;
 		while (beginTagIndex != -1) {
-			beginTagIndex = html.indexOf("<");
+			beginTagIndex = htmlBldr.indexOf("<");
 			if (beginTagIndex != -1) {
 				int endTagIndex = html.indexOf(">", beginTagIndex + 1);
-				String beforeTag = html.substring(0, beginTagIndex);
-				String afterTag = html.substring(endTagIndex + 1);
-				html = beforeTag + afterTag;
+				htmlBldr.delete(beginTagIndex, endTagIndex + 1);
 			}
+			
+			html = htmlBldr.toString();
 
 			html = html.replace("\n\n", "\n");
 		}
